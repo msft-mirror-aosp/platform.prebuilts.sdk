f261520808f6cca3c7b2046df5b7132694b8528c
Jean-Marie Henaff <jmhenaff@google.com>
2015-03-16 11:41:33 +0100

    [DO NOT MERGE] Compile using Jack
    Import Jack and Jill Brest 1.1-a11
    
    Jack version: 154400 0fd686f8f2daf4fd06246df70359cc21fd8f6100
    Jill version: 154400 f1b75a26c2aeaa2abee1025d364abe1a7cfbf9a0
    
    This includes cherry-picks of the following changes:
    40c42d4267c3f3d727c65a9931e3715cda851638
    19b3485a17e4c6823aea3f333551280f26410ac5
    191c1235d4d4b0f38f6e750aed0003e69f9aa41e
    9a0baa59ab5e6c36aa1e3759de803ba721414c76
    8a24cc37488e9897ede3587bfc5afc8426d3da3d
    e361d0b9da0fc4325c99a106af16c15dae0b3095
    1b697cb67992941835e8338087d4eb0b35dbdc5f
    30b5f3189c0ba039dfe5b9eee40b2125e8805c47
    8bfc194a7328d068d02c77b827e0a29cbcb8292e
    4b541e77beaf77912ba86067fd7ac8c3ba986572
    485334137b88289db6eb1dcbdb6e6ef31478519d
    56112a09cbf72798e8ed731a8675ca64c1b61ff3
    
    Change-Id: I1876136fe1aca1d11e8ba548de8f73a190a43fb5

--- tools/Android.mk
+++ tools/Android.mk
@@ -16,6 +16,84 @@
 
 LOCAL_PATH:= $(call my-dir)
 
+##################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := jack
+LOCAL_SRC_FILES := jack
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := $(HOST_EXECUTABLE_SUFFIX)
+LOCAL_BUILT_MODULE_STEM := jack$(HOST_EXECUTABLE_SUFFIX)
+LOCAL_IS_HOST_MODULE := true
+
+include $(BUILD_PREBUILT)
+jack_script := $(LOCAL_INSTALLED_MODULE)
+
+##################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := jack-admin
+LOCAL_SRC_FILES := jack-admin
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := $(HOST_EXECUTABLE_SUFFIX)
+LOCAL_BUILT_MODULE_STEM := jack-admin$(HOST_EXECUTABLE_SUFFIX)
+LOCAL_IS_HOST_MODULE := true
+
+include $(BUILD_PREBUILT)
+jack_admin_script := $(LOCAL_INSTALLED_MODULE)
+
+##################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := jack
+LOCAL_SRC_FILES := jack.jar
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := $(COMMON_JAVA_PACKAGE_SUFFIX)
+LOCAL_BUILT_MODULE_STEM := jack$(COMMON_JAVA_PACKAGE_SUFFIX)
+LOCAL_IS_HOST_MODULE := true
+LOCAL_ADDITIONAL_DEPENDENCIES := $(JACK_LAUNCHER_JAR)
+LOCAL_POST_INSTALL_CMD := $(hide) $(JACK_SERVER_LOG_COMMAND) JACK_VM_COMMAND="$(JACK_VM) $(JAVA_TMPDIR_ARG) $(DEFAULT_JACK_VM_ARGS) -jar $(JACK_LAUNCHER_JAR) " JACK_JAR="$(JACK_JAR)" $(jack_admin_script) start-server
+
+include $(BUILD_PREBUILT)
+$(LOCAL_INSTALLED_MODULE): $(jack_script)
+# kill if stop failed, but ignore kill errors since jack-admin is reporting "no server running" as
+# an error.
+kill_server := $(intermediates)/kill_server.stamp
+$(kill_server) : $(LOCAL_BUILT_MODULE) $(jack_admin_script)
+	$(hide) $(jack_admin_script) stop-server || $(jack_admin_script) kill-server || exit 0
+	touch $@
+
+$(LOCAL_INSTALLED_MODULE): $(kill_server)
+
+##################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := jack-launcher
+LOCAL_SRC_FILES := jack-launcher.jar
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := $(COMMON_JAVA_PACKAGE_SUFFIX)
+LOCAL_BUILT_MODULE_STEM := jack-launcher$(COMMON_JAVA_PACKAGE_SUFFIX)
+LOCAL_IS_HOST_MODULE := true
+
+include $(BUILD_PREBUILT)
+
+##################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := jill
+LOCAL_SRC_FILES := jill.jar
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := $(COMMON_JAVA_PACKAGE_SUFFIX)
+LOCAL_BUILT_MODULE_STEM := jill$(COMMON_JAVA_PACKAGE_SUFFIX)
+LOCAL_IS_HOST_MODULE := true
+
+include $(BUILD_PREBUILT)
+
 ifneq (,$(TARGET_BUILD_APPS)$(filter true,$(TARGET_BUILD_PDK)))
 
 ##################################
