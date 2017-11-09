d677b60699d840d0241c989305cc63d9a1451de3
Dan Willemsen <dwillemsen@google.com>
2017-11-08 22:13:17 -0800

    Produce resource package for support-v4 and support-v13
    These needed to switch from LOCAL_STATIC_JAVA_LIBRARIES to
    LOCAL_STATIC_ANDROID_LIBRARIES so that they actually combine resources
    as well. This means that they need an AndroidManifest.xml, so extract
    the originals and use those.
    
    Bug: 64723465
    Test: m ANDROID_BUILDSPEC=vendor/google/build/app_build_spec.mk
    Change-Id: Idfcf2c4a9df55a24007480ccb892fdc42a947744

--- current/support/Android.mk
+++ current/support/Android.mk
@@ -1,5 +1,5 @@
 # Automatically generated with:
-# pom2mk '-rewrite=^multidex-instrumentation$=android-support-multidex-instrumentation' '-rewrite=^preference-v7$=android-support-v7-preference' '-rewrite=^support-core-utils$=android-support-core-utils' '-rewrite=^support-emoji-appcompat$=android-support-emoji-appcompat' '-rewrite=^cardview-v7$=android-support-v7-cardview' '-rewrite=^support-media-compat$=android-support-media-compat' '-rewrite=^gridlayout-v7$=android-support-v7-gridlayout' '-rewrite=^support-annotations$=android-support-annotations' '-rewrite=^constraint-layout-solver$=android-support-constraint-layout-solver' '-rewrite=^exifinterface$=android-support-exifinterface' '-rewrite=^support-v13$=android-support-v13-nodeps' '-rewrite=^design$=android-support-design' '-rewrite=^support-emoji$=android-support-emoji' '-rewrite=^recommendation$=android-support-recommendation' '-rewrite=^appcompat-v7$=android-support-v7-appcompat-nodeps' '-rewrite=^recyclerview-v7$=android-support-v7-recyclerview' '-rewrite=^support-vector-drawable$=android-support-vectordrawable' '-rewrite=^wear$=android-support-wear' '-rewrite=^animated-vector-drawable$=android-support-animatedvectordrawable' '-rewrite=^support-emoji-bundled$=android-support-emoji-bundled' '-rewrite=^transition$=android-support-transition' '-rewrite=^support-dynamic-animation$=android-support-dynamic-animation' '-rewrite=^support-fragment$=android-support-fragment' '-rewrite=^customtabs$=android-support-customtabs' '-rewrite=^percent$=android-support-percent' '-rewrite=^palette-v7$=android-support-v7-palette' '-rewrite=^support-compat$=android-support-compat' '-rewrite=^multidex$=android-support-multidex' '-rewrite=^preference-v14$=android-support-v14-preference' '-rewrite=^support-core-ui$=android-support-core-ui' '-rewrite=^support-tv-provider$=android-support-tv-provider' '-rewrite=^constraint-layout$=android-support-constraint-layout' '-rewrite=^leanback-v17$=android-support-v17-leanback' '-rewrite=^preference-leanback-v17$=android-support-v17-preference-leanback' '-rewrite=^mediarouter-v7$=android-support-v7-mediarouter' .
+# pom2mk '-rewrite=^multidex-instrumentation$=android-support-multidex-instrumentation' '-rewrite=^preference-v7$=android-support-v7-preference' '-rewrite=^support-core-utils$=android-support-core-utils' '-rewrite=^support-emoji-appcompat$=android-support-emoji-appcompat' '-rewrite=^cardview-v7$=android-support-v7-cardview' '-rewrite=^support-media-compat$=android-support-media-compat' '-rewrite=^gridlayout-v7$=android-support-v7-gridlayout' '-rewrite=^support-annotations$=android-support-annotations' '-rewrite=^constraint-layout-solver$=android-support-constraint-layout-solver' '-rewrite=^exifinterface$=android-support-exifinterface' '-rewrite=^support-v13$=android-support-v13-nodeps' '-rewrite=^design$=android-support-design' '-rewrite=^support-emoji$=android-support-emoji' '-rewrite=^recommendation$=android-support-recommendation' '-rewrite=^appcompat-v7$=android-support-v7-appcompat-nodeps' '-rewrite=^recyclerview-v7$=android-support-v7-recyclerview' '-rewrite=^support-vector-drawable$=android-support-vectordrawable' '-rewrite=^wear$=android-support-wear' '-rewrite=^animated-vector-drawable$=android-support-animatedvectordrawable' '-rewrite=^support-v4$=android-support-v4-nodeps' '-rewrite=^support-emoji-bundled$=android-support-emoji-bundled' '-rewrite=^transition$=android-support-transition' '-rewrite=^support-dynamic-animation$=android-support-dynamic-animation' '-rewrite=^support-fragment$=android-support-fragment' '-rewrite=^customtabs$=android-support-customtabs' '-rewrite=^percent$=android-support-percent' '-rewrite=^palette-v7$=android-support-v7-palette' '-rewrite=^support-compat$=android-support-compat' '-rewrite=^multidex$=android-support-multidex' '-rewrite=^preference-v14$=android-support-v14-preference' '-rewrite=^support-core-ui$=android-support-core-ui' '-rewrite=^support-tv-provider$=android-support-tv-provider' '-rewrite=^constraint-layout$=android-support-constraint-layout' '-rewrite=^leanback-v17$=android-support-v17-leanback' '-rewrite=^preference-leanback-v17$=android-support-v17-preference-leanback' '-rewrite=^mediarouter-v7$=android-support-v7-mediarouter' .
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -25,6 +25,7 @@ LOCAL_BUILT_MODULE_STEM := javalib.jar
 LOCAL_MODULE_SUFFIX := .aar
 LOCAL_USE_AAPT2 := true
 LOCAL_STATIC_ANDROID_LIBRARIES := \
+  android-support-v4-nodeps \
   android-support-vectordrawable \
   android-support-animatedvectordrawable \
 
@@ -64,6 +65,7 @@ LOCAL_BUILT_MODULE_STEM := javalib.jar
 LOCAL_MODULE_SUFFIX := .aar
 LOCAL_USE_AAPT2 := true
 LOCAL_STATIC_ANDROID_LIBRARIES := \
+  android-support-v4-nodeps \
   android-support-v7-appcompat-nodeps \
   android-support-v7-recyclerview \
   android-support-transition \
@@ -163,6 +165,7 @@ LOCAL_BUILT_MODULE_STEM := javalib.jar
 LOCAL_MODULE_SUFFIX := .aar
 LOCAL_USE_AAPT2 := true
 LOCAL_STATIC_ANDROID_LIBRARIES := \
+  android-support-v4-nodeps \
   android-support-v7-appcompat-nodeps \
   android-support-v7-recyclerview \
   android-support-v7-preference \
@@ -180,6 +183,7 @@ LOCAL_BUILT_MODULE_STEM := javalib.jar
 LOCAL_MODULE_SUFFIX := .aar
 LOCAL_USE_AAPT2 := true
 LOCAL_STATIC_ANDROID_LIBRARIES := \
+  android-support-v4-nodeps \
   android-support-v7-appcompat-nodeps \
   android-support-v7-recyclerview \
   android-support-v7-preference \
@@ -195,6 +199,7 @@ LOCAL_BUILT_MODULE_STEM := javalib.jar
 LOCAL_MODULE_SUFFIX := .aar
 LOCAL_USE_AAPT2 := true
 LOCAL_STATIC_ANDROID_LIBRARIES := \
+  android-support-v4-nodeps \
   android-support-v7-appcompat-nodeps \
   android-support-v7-recyclerview \
 
@@ -380,6 +385,24 @@ LOCAL_BUILT_MODULE_STEM := javalib.jar
 LOCAL_MODULE_SUFFIX := .aar
 LOCAL_USE_AAPT2 := true
 LOCAL_STATIC_ANDROID_LIBRARIES := \
+  android-support-v4-nodeps \
+
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := android-support-v4-nodeps
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_SRC_FILES := m2repository/com/android/support/support-v4/28.0.0-SNAPSHOT/support-v4-28.0.0-20170824.012343-1.aar
+LOCAL_BUILT_MODULE_STEM := javalib.jar
+LOCAL_MODULE_SUFFIX := .aar
+LOCAL_USE_AAPT2 := true
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+  android-support-compat \
+  android-support-media-compat \
+  android-support-core-utils \
+  android-support-core-ui \
+  android-support-fragment \
 
 include $(BUILD_PREBUILT)
 
