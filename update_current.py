4f4b906dad88418d9e2bf019e16b49a712a559e8
Colin Cross <ccross@android.com>
2018-02-14 10:03:19 -0800

    Fix update_current.py
    I3baf5b22f800f35db5517c66f84e7b9c11b101c5 passed a build_id object
    instead of a string to fetch_artifact for core.current.stubs.jar,
    which then attempted to fetch from a build called
    '<__main__.buildId object at 0x7fbdfaf0c518>'.  Pass build_id.fs_id
    instead.
    
    Bug: 72206056
    Test: ./update_current.py -p 4603536
    Change-Id: Ieba4b66455477b8edbf16595a55e0812fca76eac

--- update_current.py
+++ update_current.py
@@ -265,7 +265,7 @@ def update_sdk_repo(target, build_id):
         # Unclear if this is actually necessary.
         extract_to(zipFile, paths, 'framework.aidl', system_path)
 
-    artifact_path = fetch_artifact(target, build_id, 'core.current.stubs.jar')
+    artifact_path = fetch_artifact(target, build_id.fs_id, 'core.current.stubs.jar')
     if not artifact_path:
         return False
 
