5c3b0bbcf2df147c09eab0995c0e92c076b9e438
Jiyong Park <jiyong@google.com>
2018-03-07 23:36:18 +0900

    Mark prebuilt API stubs as java:sdk rather than java:platform
    These prebuilt API stubs are used when building the unbundled apps
    (TARGET_BUILD_APPS is not empty). However, the prebuilts have been
    defined without LOCAL_SDK_VERSION, and as a result, their link-type has
    been incorrectly identified as java:platform.
    
    This is causing link-type check warning when building the unbundled
    apps. And this warning is going to be promoted to error.
    
    Fixing the error by adding LOCAL_SDK_VERSION := current or <ver> when
    defining the prebuilts. Note that the same API stubs that are built from
    the source code are already with LOCAL_SDK_VERSION set. This change
    eliminates the inconsistency.
    
    Bug: 69899800
    Test: m -j ANDROID_BUILD_SPEC=vendor/google/build/app_build_spec.mk
    Exempt-From-Owner-Approval: CP from master, approved there.
    Change-Id: I828eb1cfc1e14c7d72e2f08da7f68809348946bb
    Merged-In: I828eb1cfc1e14c7d72e2f08da7f68809348946bb
    (cherry picked from commit 1df92df41119941d8e57e803a1fd5ed4a03d08ef)

--- current/Android.mk
+++ current/Android.mk
@@ -22,19 +22,35 @@ LOCAL_PATH := $(call my-dir)
 # For apps (unbundled) build, replace the typical
 # make target artifacts with prebuilts.
 ifneq (,$(TARGET_BUILD_APPS)$(filter true,$(TARGET_BUILD_PDK)))
-include $(CLEAR_VARS)
-
 # Set up prebuilts for the core Support Library artifacts.
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += \
+support_jars := \
   $(patsubst $(LOCAL_PATH)/%,%,\
     $(shell find $(LOCAL_PATH)/support -name "*.jar"))
 
 # Set up prebuilts for Multidex library artifacts.
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += \
+multidex_jars := \
   $(patsubst $(LOCAL_PATH)/%,%,\
     $(shell find $(LOCAL_PATH)/multidex -name "*.jar"))
 
-include $(BUILD_MULTI_PREBUILT)
+prebuilts := $(foreach jar,$(support_jars) $(multidex_jars),\
+    $(basename $(notdir $(jar))):$(jar))
+
+define define-prebuilt
+    $(eval tw := $(subst :, ,$(strip $(1)))) \
+    $(eval include $(CLEAR_VARS)) \
+    $(eval LOCAL_MODULE := $(word 1,$(tw))) \
+    $(eval LOCAL_MODULE_TAGS := optional) \
+    $(eval LOCAL_MODULE_CLASS := JAVA_LIBRARIES) \
+    $(eval LOCAL_SRC_FILES := $(word 2,$(tw))) \
+    $(eval LOCAL_UNINSTALLABLE_MODULE := true) \
+    $(eval LOCAL_SDK_VERSION := current) \
+    $(eval include $(BUILD_PREBUILT))
+endef
+
+$(foreach p,$(prebuilts),\
+    $(call define-prebuilt,$(p)))
+
+prebuilts :=
 
 # Generates the v4, v13, and appcompat libraries with static dependencies.
 include $(call all-makefiles-under,$(LOCAL_PATH))
