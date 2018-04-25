3eb3bfcc7babc8bce565787ac48dcfedb5a12871
Jeff Gaston <jeffrygaston@google.com>
2018-04-25 19:43:35 -0400

    Make pom2mk argument order consistent
    To prevent merge conflicts in case update_prebuilts.py is executed separately on separate branches
    Bug: 76088091
    
    Change-Id: I96b109405e0d503dee064d7e3faad64712e02c6b
    Merged-In: I96b109405e0d503dee064d7e3faad64712e02c6b

--- update_prebuilts/update_prebuilts.py
+++ update_prebuilts/update_prebuilts.py
@@ -428,7 +428,7 @@ def transform_maven_repos(maven_repo_dirs, transformed_dir, extract_res=True, in
         args = ["pom2mk", "-sdk-version", "current"]
         if include_static_deps:
             args.append("-static-deps")
-        rewriteNames = [name for name in maven_to_make if ":" in name] + [name for name in maven_to_make if ":" not in name]
+        rewriteNames = sorted([name for name in maven_to_make if ":" in name] + [name for name in maven_to_make if ":" not in name])
         args.extend(["-rewrite=^" + name + "$=" + maven_to_make[name][0] for name in rewriteNames])
         args.extend(["-extra-deps=android-support-car=prebuilt-android.car-stubs"])
         args.extend(["."])
