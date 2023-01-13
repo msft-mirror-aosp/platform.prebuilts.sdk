b3eeedd2ee1bf61616d6df6cc7d9915344d0f840
Eric Miao <ericymiao@google.com>
2023-01-13 09:37:42 -0800

    Update sdk target to sdk-sdk_mac for builtools
    Change-Id: I8ae43a361c40c85ccbca69444139444245a136a6

--- update_prebuilts/update_prebuilts.py
+++ update_prebuilts/update_prebuilts.py
@@ -1099,7 +1099,7 @@ def main():
                 build_id=build_id.url_id)
             subprocess.check_call(shlex.split(cmd), cwd=repo_root_dir.resolve())
         if args.buildtools:
-            if update_buildtools('sdk_mac', 'darwin', build_id, args.beyond_corp) \
+            if update_buildtools('sdk-sdk_mac', 'darwin', build_id, args.beyond_corp) \
                     and update_buildtools('sdk', 'linux', build_id, args.beyond_corp) \
                     and update_buildtools('sdk', 'windows', build_id, args.beyond_corp):
                 components.append('build tools')
